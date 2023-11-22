de4b381a2c1727e59fe37914e4106beda4591d8d
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-11-22 16:03:16 -0500

    ver02 actualizado

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+}) -> name('xInicio');
+
+Route::get('/saludo', function () {
+    return "Hola mundo desde laravel...";
 });
+
+Route::get('/galaria/{num}', function ($num) {
+    return "Este es el codigo de la foto desde laravel: ".$num;
+}) -> where('num', '[0-9]+');
+
+Route::view('/galeria', 'pagGaleria', ['valor' => 15]) -> name('xGaleria'); 
+
+Route::get('/lista', function (){
+    return view('pagLista');
+}) -> name('xLista');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
